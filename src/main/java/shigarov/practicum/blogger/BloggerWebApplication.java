f3983241be7d5968dfcba802f3a93b4fc2b985f0
Alexey O. Shigarov <shigarov@gmail.com>
2025-03-09 18:57:16 +0800

    update
    Разделить профили

--- src/main/java/shigarov/practicum/blogger/BloggerWebApplication.java
+++ src/main/java/shigarov/practicum/blogger/BloggerWebApplication.java
@@ -1,10 +1,12 @@
 package shigarov.practicum.blogger;
 
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+
 import shigarov.practicum.blogger.storage.StorageProperties;
 import shigarov.practicum.blogger.storage.StorageService;
 
@@ -16,12 +18,12 @@ public class BloggerWebApplication {
 		SpringApplication.run(BloggerWebApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner init(StorageService storageService) {
-		return (args) -> {
-			storageService.deleteAll();
-			storageService.init();
-		};
+	@Autowired
+	StorageService storageService;
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void initStorage() {
+		storageService.init();
 	}
 
 }
---
+++ src/main/resources/data-prod.sql
@@ -0,0 +1,8 @@
+---- 6 тестовых тегов
+
+INSERT INTO tags (id, name) VALUES (1, 'технологии');
+INSERT INTO tags (id, name) VALUES (2, 'блог');
+INSERT INTO tags (id, name) VALUES (3, 'программирование');
+INSERT INTO tags (id, name) VALUES (4, 'жизнь');
+INSERT INTO tags (id, name) VALUES (5, 'тест');
+INSERT INTO tags (id, name) VALUES (6, 'котики');
